d1472257580f3691b39af9fff27f9ccbe67ba084
Jim Gay <jim@saturnflyer.com>
2009-04-03 00:31:36 -0400

    update user areas, updated styles

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,14 +1,13 @@
 class Admin::DashboardController < ApplicationController
-
   def index
-    @current_user_updated_pages = current_user.pages.recently_updated.find(:all, :limit => 10)
-    @current_user_draft_pages = current_user.pages.drafts.find(:all, :limit => 10)
+    @current_user_published_pages = current_user.pages.published.recently_updated.find(:all, :limit => 10)
+    @current_user_draft_pages = current_user.pages.drafts.by_updated.find(:all, :limit => 10)
     
     @updated_pages = Page.recently_updated.find(:all, :limit => 10)
-    @draft_pages = Page.drafts.find(:all, :limit => 10)
-    @reviewed_pages = Page.reviewed.find(:all, :limit => 10)
+    @draft_pages = Page.drafts.by_updated.find(:all, :limit => 10)
+    @reviewed_pages = Page.reviewed.by_updated.find(:all, :limit => 10)
     
-    @updated_snippets = Snippet.recently_updated.find(:all, :limit => 10)
-    @updated_layouts = Layout.recently_updated.find(:all, :limit => 10)
+    @updated_snippets = Snippet.recently_updated.find(:all, :limit => 10, :order => 'updated_at DESC') # default_order override
+    @updated_layouts = Layout.recently_updated.find(:all, :limit => 10, :order => 'updated_at DESC') # default_order override
   end
 end
--- dashboard_extension.rb
+++ dashboard_extension.rb
@@ -2,7 +2,7 @@
 # require_dependency 'application'
 
 class DashboardExtension < Radiant::Extension
-  version "1.0"
+  version "1.1"
   description "Dashboard provides a way to view recent activity in Radiant and gives small extensions a place to grow."
   url "http://saturnflyer.com/"
   
@@ -17,17 +17,21 @@ class DashboardExtension < Radiant::Extension
     end
     admin.dashboard = load_default_dashboard_regions
     User.class_eval {
-      has_many :created_pages, :foreign_key => :created_by_id, :class_name => 'Page' unless self.respond_to?(:created_pages)
-      has_many :updated_pages, :foreign_key => :updated_by_id, :class_name => 'Page' unless self.respond_to?(:updated_pages)
       has_many :pages, :foreign_key => :created_by_id unless self.respond_to?(:pages)
     }
     Page.class_eval {
       named_scope :drafts, :conditions => ['status_id = ?',Status['Draft'].id]
-      named_scope :recently_updated, lambda{{:conditions => ['updated_at > ?', 1.week.ago ]}}
-      named_scope :reviewed, lambda{{:conditions => ["status_id = ?", Status['Reviewed'].id ]}}
+      named_scope :reviewed, :conditions => ["status_id = ?", Status['Reviewed'].id ]
+      named_scope :published, :conditions => ["status_id = ?", Status['Published'].id ]
+      named_scope :recently_updated, lambda{{:conditions => ['updated_at > ?', 1.week.ago ], :order => 'updated_at DESC'}}
+      named_scope :by_updated, :order => 'updated_at DESC'
     }
-    Snippet.class_eval { named_scope :recently_updated, lambda{{:conditions => ['updated_at > ?', 1.week.ago ]}} }
-    Layout.class_eval { named_scope :recently_updated, lambda{{:conditions => ['updated_at > ?', 1.week.ago ]}} }
+    Snippet.class_eval { 
+      named_scope :recently_updated, lambda{{:conditions => ['updated_at > ?', 1.week.ago ], :order => 'updated_at DESC, name'}} 
+    }
+    Layout.class_eval { 
+      named_scope :recently_updated, lambda{{:conditions => ['updated_at > ?', 1.week.ago ], :order => 'updated_at DESC'}}
+    }
   end
   
   def deactivate
@@ -39,9 +43,11 @@ class DashboardExtension < Radiant::Extension
   def load_default_dashboard_regions
     returning OpenStruct.new do |dashboard|
       dashboard.index = Radiant::AdminUI::RegionSet.new do |index|
-        index.main.concat %w{header current_user_draft_pages current_user_updated_pages draft_pages reviewed_pages updated_pages updated_snippets updated_layouts}
+        index.main.concat %w{header current_user_draft_pages current_user_published_pages draft_pages reviewed_pages updated_pages updated_snippets updated_layouts}
+        index.current_user_draft_pages_top.concat %w{}
         index.current_user_draft_pages_bottom.concat %w{}
-        index.current_user_updated_pages_bottom.concat %w{}
+        index.current_user_published_pages_top.concat %w{}
+        index.current_user_published_pages_bottom.concat %w{}
         index.extensions.concat %w{}
       end
     end
